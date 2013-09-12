695a62662fd13e94abfb5ae6e2475b60c93ac6f1
Fathima Dilhasha <dilhasha.uom@gmail.com>
2013-09-12 23:22:40 +0530

    My NIC Interpreter
    110128J M.N.F.Dilhasha

---
+++ nic-interpreter/nic-interpreter/src/main/java/cse/maven/sample/InvalidNicException.java
@@ -0,0 +1,14 @@
+package cse.maven.sample;
+
+
+/**
+ *
+ * @author 110128J M.N.F.Dilhasha
+ */
+public class InvalidNicException extends Exception {
+    
+    public InvalidNicException() {
+		super("Invalid NIC number");
+	}
+    
+}
